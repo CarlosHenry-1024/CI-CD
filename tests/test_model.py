24a599ba63bb16de0185875f4a9acddfa6e24fc2
CarlosHenry-1024 <carlos.henry.hb@gmail.com>
2025-01-29 16:21:42 +0300

    test_CI/CD

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,6 +1,5 @@
-# from app.model import predict
+from app.model import predict
 
 def test_predict():
-    # result = predict("I love this product!")
-    # assert result["label"] in ["POSITIVE", "NEGATIVE"]
-    assert 'POSITIVE' in ["POSITIVE", "NEGATIVE"]
+    result = predict("I love this product!")
+    assert result["label"] in ["POSITIVE", "NEGATIVE"]
